e2a6c7d9084b7a44b31003504630dd52e5538c8a
Mr.Green <mrgreens.amazingemail@gmail.com>
2018-11-18 18:39:57 -0500

    finished the game

--- javascript/main.js
+++ javascript/main.js
@@ -1,8 +1,12 @@
 let body = document.getElementsByName("body");
-
+let timeSpent = 0;
 function whatTimeSeen() {
   let date = new Date();
   document.getElementById("time").innerHTML = date.toLocaleTimeString();
 }
-
+function timeHere() {
+  timeSpent++
+  if(timeSpent>3600) { document.getElementById("youHere").innerHTML = "You have been here for " + timeSpent + " seconds.<br> It's time to leave."
+} else {document.getElementById("youHere").innerHTML = "You have been here for " + timeSpent + " seconds."};
+}
 body.addEventListener("load", setInterval(whatTimeSeen, 10))
--- javascript/p5Game.js
+++ javascript/p5Game.js
@@ -1,8 +1,32 @@
+var speedx = 1.1;
+var speedy = 1;
+var x = 200;
+var y = 200;
+var edgeCount = 0;
+var edges = document.getElementById("cornerTouch");
 function setup() {
-  createCanvas(400, 400);
+  var myCanvas = createCanvas(400, 400);
+  myCanvas.parent('game');
 }
-
 function draw() {
-  background(180);
-  ellipse(mouseX, mouseY, 25);
+    background(100)
+  fill(0);
+  rect(x, y, 20, 20);
+  if (y < 0) {
+    speedy = random(1, 1.5);
+  } else if (y > 380) {
+    speedy = random(-1.5, -1)}
+  if (x < 0) {
+    speedx = random(1, 1.5);
+  } else if (x > 380) {
+    speedx = random(-1.5, -1);
+}
+    x = x + speedx;
+    y = y + speedy;
+if ((x == 0 && y == 0) ||
+    (x == 0 && y == 380) ||
+    (x == 380 && y == 0) ||
+    (x == 380 && y == 380)) {
+  edgeCount += 1;}
+  edges.innerHTML = "The cube has touched the corner "+edgeCount+" times.";
 }
