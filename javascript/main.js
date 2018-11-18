e5cac5487fe87c84bc0ec789fe9be71c01e692ad
Mr.Green <mrgreens.amazingemail@gmail.com>
2018-11-18 15:56:46 -0500

    lot of stuff, mostly the p5 game. Base conditions for the game set

--- javascript/main.js
+++ javascript/main.js
@@ -1,4 +1,8 @@
+let body = document.getElementsByName("body");
+
 function whatTimeSeen() {
   let date = new Date();
   document.getElementById("time").innerHTML = date.toLocaleTimeString();
 }
+
+body.addEventListener("load", setInterval(whatTimeSeen, 10))
---
+++ javascript/p5Game.js
@@ -0,0 +1,8 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(180);
+  ellipse(mouseX, mouseY, 25);
+}
