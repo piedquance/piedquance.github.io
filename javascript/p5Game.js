9b9b5d1b0ca951e610bf852ae0878619e604b1a5
Mr.Green <mrgreens.amazingemail@gmail.com>
2018-11-20 22:27:58 -0500

    added stuff.html

--- javascript/p5Game.js
+++ javascript/p5Game.js
@@ -5,26 +5,16 @@ var y = 10;
 var edgeCount = 0;
 var edges = document.getElementById("cornerTouch");
 function setup() {
-  
 if(windowWidth < 450) { sketchS = windowWidth/1.5;
   } else { sketchS = windowWidth/4.2;}
-
-
 var myCanvas = createCanvas(sketchS, sketchS);
- 
  myCanvas.parent('game');
 }
-
 function draw() {
-
  rectS = sketchS/20;
-
  background(170)
- 
  fill(0);
- 
  rect(x, y, rectS, rectS);
-
 if (y < 0) {
     speedy = random(1, 1.5);
   } else if (y > (sketchS - rectS)) {
@@ -34,11 +24,8 @@ if (y < 0) {
   } else if (x > (sketchS - rectS)) {
     speedx = random(-1.5, -1);
 }
-  
   x = x + speedx;
-  
   y = y + speedy;
-  
 if ((x == 0 && y == 0) ||
     (x == 0 && y == 380) ||
     (x == 380 && y == 0) ||
