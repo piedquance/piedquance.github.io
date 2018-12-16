0c28ae1b6143d8f1971f3ba7519e14d2dc510f6b
Mr.Green <mrgreens.amazingemail@gmail.com>
2018-12-16 10:23:54 -0500

    size shenanigans

--- javascript/cube.js
+++ javascript/cube.js
@@ -1,7 +1,10 @@
 var y = 0.001;
 var x = 0.001;
 function setup() {
-  var myCanvas = createCanvas(windowWidth/3, windowWidth/3, WEBGL);
+/*  if(windowWidth < 450) { sketchS = windowWidth/2;
+  } else { sketchS = windowWidth/4;}
+  var myCanvas = createCanvas(sketchS, sketchS, WEBGL);*/
+  var myCanvas = createCanvas(350, 350, WEBGL);
    myCanvas.parent('cube');
  }
 function draw() {
@@ -10,7 +13,7 @@ function draw() {
     background(255);
     fill(0,255,100,100);
 		stroke(5);
-    box(windowWidth/6);
+    box(150);
     x += random(0.001, 0.005);
     y += random(0.001, 0.005);
   }
