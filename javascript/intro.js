ac9c32d735612a8e0acef12c943df4438e08a35c
Mr.Green <mrgreens.amazingemail@gmail.com>
2018-12-19 17:49:08 -0500

    got some scrolling action

--- javascript/intro.js
+++ javascript/intro.js
@@ -1,3 +1,10 @@
 function replace(text) {
   text.innerHTML = "Vous ne voyez pas le texte entre crochets (balises) car le navigateur web affiche seulement le contenu (couleur noire). Le texte mauve indique le type d'élément et le texte vert indique un attribut de l'élément, qui comprend une valeur. Chaque élément a une balise d'ouverture et une balise de fermeture Pour donner un exemple, le paragraphe que vous êtes en train de lire est entouré de balises   &ltp&gt et   &lt/p&gt, qui veulent dire paragraphe. Il a aussi un attribut id='banane', cela lui assigne l'identifiant banane. Un identifiant permet de modifier l'élément en question sans affecter le reste des éléments du même type.<br>Avec JavaScript, il est possible de modifier le contenu d'éléments html directement dans le navigateur. Pour faire cela, il vous faudra accéder à la console JavaScript en faisant Ctrl-Shift-J (Windows/Linux) ou Cmd+Opt+J (Mac). Notez que la console ne sera pas assessible si vous utilisez un téléphone intelligent. Une fois là, entrez le texte suivant dans la console :"
 }
+let progress = document.getElementById("bar");
+window.addEventListener('scroll', function() {
+  let max = document.body.clientHeight - window.innerHeight;
+  let value = window.pageYOffset;
+  document.getElementById("bar").max = max;
+  document.getElementById("bar").value = value;
+});
