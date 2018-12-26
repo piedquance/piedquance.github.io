56c0dafe7bb02563fc8f439164ce533ac56c78f6
Mr.Green <mrgreens.amazingemail@gmail.com>
2018-12-26 14:01:32 -0500

    link color #3ff936

--- javascript/intro.js
+++ javascript/intro.js
@@ -1,5 +1,5 @@
 function replace(text) {
-  text.innerHTML = "Vous ne voyez pas le texte entre crochets (balises) car le navigateur web affiche seulement le contenu (couleur noire). Le texte mauve indique le type d'élément et le texte vert indique un attribut de l'élément, qui comprend une valeur. Chaque élément a une balise d'ouverture et une balise de fermeture Pour donner un exemple, le paragraphe que vous êtes en train de lire est entouré de balises   &ltp&gt et   &lt/p&gt, qui veulent dire paragraphe. Il a aussi un attribut id='banane', cela lui assigne l'identifiant banane. Un identifiant permet de modifier l'élément en question sans affecter le reste des éléments du même type.<br>Avec JavaScript, il est possible de modifier le contenu d'éléments html directement dans le navigateur. Pour faire cela, il vous faudra accéder à la console JavaScript en faisant Ctrl-Shift-J (Windows/Linux) ou Cmd+Opt+J (Mac). Notez que la console ne sera pas assessible si vous utilisez un téléphone intelligent. Une fois là, entrez le texte suivant dans la console :"
+  text.innerHTML = "Le navigateur web affiche seulement le texte noir sur l'écran, le reste sert à la structure. Le texte mauve indique le type d'élément et le texte vert indique un attribut de l'élément, qui comprend une valeur. Chaque élément a une balise d'ouverture et une balise de fermeture. Pour donner un exemple, le paragraphe que vous êtes en train de lire est entouré des balises &ltp&gt et &lt/p&gt, qui veulent dire paragraphe. Il a aussi un attribut id=\"banane\" , cela lui assigne l'identifiant banane. Un identifiant permet de modifier l'élément en question sans affecter le reste des éléments du même type. Avec JavaScript, il est possible de modifier le contenu d'éléments html directement dans le navigateur. Pour faire cela, il vous faudra accéder à la console JavaScript en faisant Ctrl-Shift-J (Windows/Linux) ou Cmd+Opt+J (Mac). Notez que la console ne sera pas assessible si vous utilisez un téléphone intelligent. Une fois là, entrez le texte suivant dans la console :"
 }
 let progress = document.getElementById("bar");
 window.addEventListener('scroll', function() {
@@ -17,12 +17,20 @@ document.getElementById("22"),
 document.getElementById("33"),
 document.getElementById("44"),
 document.getElementById("55")];
+function asdf(x) {
+  if (window.pageYOffset > p[x+1].getBoundingClientRect().top + window.scrollY) {  return a[x].style.color = "#aaa"
+} else return a[x].style.color = ""
+};
 window.addEventListener("scroll", function() {
-  try {
-  for (var v = 0; v <= p.length; v++) {
-    if (window.pageYOffset > p[v+1].getBoundingClientRect().top + window.scrollY) {
-      a[v].style.color = "#aaa"
-    }
+try {
+  for (let x = 0; x <= p.length; x++) {
+    a[x].addEventListener("mouseover", function() {
+      this.style.color = "#3ff936";
+    });
+    a[x].addEventListener("mouseout", function() {
+      asdf(x);
+    });
+    asdf(x);
   }} catch(TypeError) {}
 });
 var menu = document.getElementById("menu");
