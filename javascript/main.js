74e0dc340aed0bddedd4d038a7d05223ca67794b
Mr.Green <mrgreens.amazingemail@gmail.com>
2018-12-31 19:59:11 -0500

    yeet things are working

--- javascript/main.js
+++ javascript/main.js
@@ -1,9 +1,16 @@
 let body = document.getElementsByName("body");
 let timeSpent = 0;
 function whatTimeSeen() {
-  let date = new Date();
-  document.getElementById("time").innerHTML = date.toLocaleTimeString();
+  let select = document.getElementById("selectDate").value;
+  if (select === "now") {
+    let date = new Date();
+    document.getElementById("time").innerHTML = date.toLocaleTimeString();
+  } else if (select === "year") {
+
+    document.getElementById("time").innerHTML = "yeet"
+  }
 }
+
 function timeHere() {
   timeSpent++
   if(timeSpent>3600) { document.getElementById("youHere").innerHTML = "You have been here for " + timeSpent + " seconds.<br> It's time to leave."
