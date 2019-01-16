183b744ec90adecbda761ad31da9d1041c15ec86
Mr.Green <mrgreens.amazingemail@gmail.com>
2019-01-15 20:59:05 -0500

    image 1

--- javascript/p5Game.js
+++ javascript/p5Game.js
@@ -2,20 +2,24 @@ var speedx = 1.1;
 var speedy = 1;
 var x = 10;
 var y = 10;
+var z = 10;
 var edgeCount = 0;
 var edges = document.getElementById("cornerTouch");
 function setup() {
 if(windowWidth < 450) { sketchS = windowWidth/1.5;
-  } else { sketchS = windowWidth/4.2;}
+} else { sketchS = windowWidth/4;}
 var myCanvas = createCanvas(sketchS, sketchS);
  myCanvas.parent('game');
+ rectS = sketchS/20;
 }
 function draw() {
- rectS = sketchS/20;
- background(200, 30);
- noStroke();
- fill(0, 100);
- rect(x, y, rectS, rectS);
+clear();
+noStroke();
+fill(0, 160);
+rect(0, 0, sketchS, y);
+rect(0, y, x, rectS);
+rect(x + rectS, y, sketchS, rectS);
+rect(0, y + rectS, sketchS, sketchS);
 if (y < 0) {
     speedy = random(1, 1.5);
   } else if (y > (sketchS - rectS)) {
@@ -25,12 +29,12 @@ if (y < 0) {
   } else if (x > (sketchS - rectS)) {
     speedx = random(-1.5, -1);
 }
-  x = x + speedx;
-  y = y + speedy;
 if ((x == 0 && y == 0) ||
-    (x == 0 && y == 380) ||
-    (x == 380 && y == 0) ||
-    (x == 380 && y == 380)) {
+    (x == 0 && y == sketchS- rectS) ||
+    (x == sketchS- rectS && y == 0) ||
+    (x == sketchS- rectS && y == sketchS- rectS)) {
   edgeCount += 1;}
   edges.innerHTML = "The square has touched the corner "+edgeCount+" times.";
+  x = round((x + speedx) *z) /z;
+  y = round((y + speedy)*z) /z;
 }
