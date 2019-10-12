c91af04a4735bb27f5cf5f19d545f47c19910149
shutterstacks <shutterstacks@gmail.com>
2019-10-12 16:00:41 -0400

    it looks better now

--- javascript/square.js
+++ javascript/square.js
@@ -6,16 +6,19 @@ var z = 10;
 var edgeCount = 0;
 var edges = document.getElementById("cornerTouch");
 function setup() {
-if(windowWidth < 450) { sketchS = windowWidth/1.5;
-} else { sketchS = 300;}
+  if(windowWidth < 950 || windowHeight < 300) { sketchS = 0;
+  } else { sketchS = windowWidth/5.5;}
 var myCanvas = createCanvas(sketchS, sketchS);
  myCanvas.parent('game');
  rectS = sketchS/20;
 }
 function draw() {
+  if(windowWidth < 950|| windowHeight < 300) { sketchS = 0;
+  } else { sketchS = windowWidth/5.5;}
+  myCanvas = createCanvas(sketchS, sketchS);
 clear();
 noStroke();
-fill(0, 150);
+fill(0, 153);
 rect(0, 0, sketchS, y);
 rect(0, y, x, rectS);
 rect(x + rectS, y, sketchS, rectS);
@@ -32,7 +35,31 @@ if (y < 0) {
   x = round((x + speedx) *z) /z;
   y = round((y + speedy)*z) /z;
 }
+var a = 1;
+var stuff = document.getElementsByClassName("stuff")[0].style;
+var info = document.getElementsByClassName("info")[0].style;
+var img = document.body.style;
+
 document.getElementsByName("body").addEventListener("load", setInterval(() => {
    let date = new Date();
-   document.getElementById("time").innerHTML = date.toLocaleTimeString();
-}, 10));
+   if ( windowWidth > 950) {
+    document.getElementById("time").innerHTML = date.toLocaleTimeString();
+    stuff.width = "39.09090909%";
+    stuff.left = "0.9090909%";
+    info.width = "39.09090909%";
+    info.right = "0.9090909%";
+   } else {
+    document.getElementById("time").innerHTML = "";
+   stuff.width = "50%";
+   stuff.left = "0";
+   info.width = "50%";
+   info.right = "0";
+   } 
+   if( windowHeight > 813) {
+      img.backgroundSize = "auto " + windowHeight + "px";
+   } else {
+    img.backgroundSize = "auto";
+   }
+}, 100));
+
+document.getElementsByClassName(li)[0].addEventListener("mouseover", () => {document.getElementsByClassName(li)[0].innerHTML += "...yet"});
